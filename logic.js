79002f335ccda624a762a7e5b8b1f3ca8e788df3
Vsein <vseinssj@gmail.com>
2022-09-20 21:27:52 +0300

    Submit first working version
    Pending features are:
    1. Proper floating point numbers handling
    2. Keyboard support
    3. Design?

--- logic.js
+++ logic.js
@@ -1,44 +1,107 @@
-const add = function(a, b) {
+function add(a, b) {
   return a + b;
-};
+}
 
-const subtract = function(a, b) {
+function subtract(a, b) {
   return a - b;
 };
 
-const multiply = function(a, b) {
+function multiply(a, b) {
   return a * b;
 }
 
-const divide = function(a, b) {
+function divide(a, b) {
   return a / b;
 }
 
-const power = function(a, b) {
-  return a ** b;
-};
-
-const factorial = function(n) {
-  let product = 1;
-  for (let i = 1; i <= n; i++) {
-    product *= i;
-  }
-  return product;
-};
-
-const operate(operator, a, b) {
+function operate(operator, a, b) {
   if (operator == '+') return add(a, b);
   else if (operator == '-') return subtract(a, b);
   else if (operator == '*') return multiply(a, b);
-  else if (operator == '/') return multiply(a, b);
+  else if (operator == '/') return (b === 0 ? 80085 : divide(a, b));
 }
 
-// Do not edit below this line
-module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
-};
+function getLast(string) {
+  return string.slice(-1);
+}
+
+function hasAnOperator(string) {
+  let operators = ['+', '-', '*', '/'];
+  return operators.indexOf(getLast(string)) !== -1;
+}
+
+function hasADot(string) {
+  return string.indexOf('.') !== -1;
+}
+
+function addDigit(e) {
+  const display = document.querySelector('.display p');
+  if (hasAnOperator(display.textContent)) {
+    display.nextElementSibling.textContent += this.id;
+  } else {
+    display.textContent += this.id;
+  }
+}
+
+function addDot(e) {
+  const display = document.querySelector('.display p');
+  if (hasAnOperator(display.textContent)) {
+    const next = display.nextElementSibling;
+    if (hasADot(next.textContent)) return;
+    next.textContent += '.';
+  } else {
+    if (hasADot(display.textContent)) return;
+    display.textContent += '.';
+  }
+}
+
+function addOperation(e) {
+  const display = document.querySelector('.display p');
+  if (hasAnOperator(display.textContent)) calc();
+  display.textContent += this.textContent;
+}
+
+function clear() {
+  const display = document.querySelector('.display p');
+  display.textContent = '';
+  console.log(display.nextElementSibling);
+  display.nextElementSibling.textContent = '';
+}
+
+function del() {
+  const display = document.querySelector('.display p');
+  let a = display.textContent;
+  let b = display.nextElementSibling.textContent;
+  if (b !== '') {
+    display.nextElementSibling.textContent = b.slice(0, -1);
+  } else if (a !== '') {
+    display.textContent = a.slice(0, -1);
+  } else {
+    return;
+  }
+}
+
+function calc() {
+  const display = document.querySelector('.display p');
+  let a = display.textContent;
+  let b = display.nextElementSibling.textContent;
+  if (a === '') return;
+  if (b === '') {
+    display.textContent = (hasAnOperator(a) ? a.slice(0, -1) : a);
+    return;
+  }
+  display.textContent = operate(getLast(a), +a.slice(0, -1), +b);
+  display.nextElementSibling.textContent = '';
+  return;
+}
+
+const numbers = document.querySelectorAll('.num');
+numbers.forEach(num => num.addEventListener('click', addDigit));
+
+const operators = document.querySelectorAll('.operation');
+operators.forEach(operator => operator.addEventListener('click', addOperation));
+
+document.querySelector('#clear').addEventListener('click', clear);
+document.querySelector('#equals').addEventListener('click', calc);
+document.querySelector('#delete').addEventListener('click', del);
+document.querySelector('#dot').addEventListener('click', addDot);
